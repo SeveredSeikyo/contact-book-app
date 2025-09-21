c68287d1118f5fcd4dcefca892bbc6a5a50f42e4
severedseikyo <thanmaysadguru5912@gmail.com>
2025-09-21 22:53:24 +0530

    New API routes Added

--- backend/src/models/contact.model.js
+++ backend/src/models/contact.model.js
@@ -39,4 +39,52 @@ const runDeleteQuery = ( query, params )=>{
     });
 }
 
-module.exports = {runPostQuery, runGetQuery, runDeleteQuery};
+const runUpdateQuery = ( query, params )=>{
+    return new Promise((resolve,reject)=>{
+        db.run(query,params,(err,row)=>{
+            if(err){
+                console.log(err.message);
+                reject(err);
+            }else{
+                resolve(row);
+            }
+        });
+    });
+}
+
+const runSearchQuery = ( query, params )=>{
+    return new Promise((resolve,reject)=>{
+        db.all(query,params,(err,rows)=>{
+            if(err){
+                console.log(err.message);
+                reject(err);
+            }else{
+                resolve(rows);
+            }
+        });
+    });
+}
+
+const runFetchQuery = ( query, params )=>{
+    return new Promise((resolve,reject)=>{
+        db.get(query,params,(err,row)=>{
+            if(err){
+                console.log(err.message);
+                reject(err);
+            }else{
+                resolve(row);
+            }
+        });
+    });
+}
+
+
+
+module.exports = {
+    runPostQuery, 
+    runGetQuery, 
+    runDeleteQuery, 
+    runUpdateQuery, 
+    runSearchQuery,
+    runFetchQuery
+};
--- backend/src/routes/contact.routes.js
+++ backend/src/routes/contact.routes.js
@@ -2,9 +2,17 @@ const express = require('express');
 const db = require('../config/db');
 const uuid = require('uuid');
 const contactRouter = express.Router();
-const {runPostQuery, runGetQuery, runDeleteQuery} = require('../models/contact.model');
+const {
+    runPostQuery, 
+    runGetQuery, 
+    runDeleteQuery, 
+    runUpdateQuery,
+    runFetchQuery,
+    runSearchQuery
+} = require('../models/contact.model');
 
-//DB Queries
+
+// DB Queries
 const insertContactQuery = `
     INSERT INTO contacts( uuid, name, email, phone ) VALUES( ?, ?, ?, ?);
 `
@@ -21,14 +29,29 @@ const deleteContactQuery = `
     DELETE FROM contacts WHERE uuid = ?;
 `
 
-//Validators
+const updateContactQuery = `
+    UPDATE contacts SET name = ?, email = ?, phone = ? 
+    WHERE uuid = ?; 
+`
+
+const searchContactsQuery = `
+    SELECT uuid, name, email, phone FROM contacts 
+    WHERE name LIKE ? OR email LIKE ? OR phone LIKE ?;
+`
+
+const fetchContactQuery = `
+    SELECT * FROM contacts WHERE uuid = ?;
+`
+
+
+// Validators
 const emailRegexp = /^\S+@\S+\.\S+$/;
 const mobileRegexp = /^\d{10}$/;
 
 
-//API ENDPOINTS
+// API ENDPOINTS
 
-//1. POST /contacts
+// 1. POST /contacts
 
 contactRouter.post('/contacts',async (req,res)=>{
     const contactFormData = req.body;
@@ -58,7 +81,7 @@ contactRouter.post('/contacts',async (req,res)=>{
     
 })
 
-//2. GET /contacts
+// 2. GET /contacts
 
 contactRouter.get('/contacts',async (req,res)=>{
     let {limit, page} = req.query;
@@ -88,12 +111,13 @@ contactRouter.get('/contacts',async (req,res)=>{
     }
 });
 
-//3. DELETE /contacts/:id
+// 3. DELETE /contacts/:id
 
 contactRouter.delete('/contact/:id',async (req,res)=>{
     const id = req.params.id;
     try{
         await runDeleteQuery(deleteContactQuery, id)
+        res.status(201)
         res.send(
             {
                 message: "Contact Deleted Successfully",
@@ -121,24 +145,73 @@ contactRouter.post('/contacts/list',async (req,res)=>{
         if(emailRegexp.test(email) && mobileRegexp.test(phone)){
             try{
                 await runPostQuery(insertContactQuery,[uniqueId,name,email,phone]);
-                
-                res.status(201);
-                res.send({
-                    message: "Contact Inserted Successfully"
-                });
             }catch(err){
-                res.status(400);
-                res.send({
-                    message: err.message
-                });
+                console.log(err)
             }
         }
         else{
-            res.status(400);
-            res.send("Invalid Constraints");
+            console.log("Invalid");
         }
         
-    });    
+    });
+    res.status(201);
+    res.send({
+        message: "All Contacts Added"
+    })    
+});
+
+// 5. PATCH /contacts/:id
+
+contactRouter.patch('/contact/:id',async (req,res)=>{
+    const id = req.params.id;
+    const { name, email, phone } = req.body;
+    try{
+        const contact = await runFetchQuery(fetchContactQuery,id);
+        
+        if(contact){
+            await runUpdateQuery(updateContactQuery, [name, email, phone, id]);
+            res.status(201);
+            res.send({
+                message: "Contact Updated Successfully"
+            })
+        }else{
+            res.status(402);
+            res.send({
+                message: "Contact does not exist"
+            });
+        }
+
+    }catch(err){
+        res.status(400);
+        res.send({
+            message: err.message
+        })
+    }
+});
+
+// 6. GET /contacts/search
+
+contactRouter.get('/contacts/search', async(req,res)=>{
+
+    const { text } = req.query
+    const queryText = `%${text}%`
+    
+    console.log(queryText);
+
+    try{
+        const contacts = await runSearchQuery( 
+            searchContactsQuery, 
+            [ queryText, queryText, queryText ]
+        );
+        console.log(contacts);
+    }catch(err){
+        res.status(400)
+        res.send({
+            message: err.message
+        })
+    }
+
 })
+
 
 module.exports = contactRouter;
